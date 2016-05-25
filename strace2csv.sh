347540a9939a64814870437c8a03ec34da05bf6a
Alejandro <acaldero.uc3m@gmail.com>
2016-05-25 23:27:13 +0200

    strace2csv improvements

--- strace2csv.sh
+++ strace2csv.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 
 #
-# strace2csv (version 2.1)
+# strace2csv (version 2.2)
 # Alejandro Calderon @ ARCOS.INF.UC3M.ES
 # GPL 3.0
 #
@@ -13,200 +13,288 @@
 import os 
 import re 
 import sys
+import math
+import time
+import getopt
 
 
-# 
-# Names (an), Types (ak), and Operations (ao) 
-# 
+def print_records ( format, data ):
+        if (format == 'json'):
+            for y in data:
+ 	        print y
 
-an = {} 
-an[0] = '"stdin"'
-an[1] = '"stdout"'
-an[2] = '"stderr"'
-
-ak = {} 
-ak[0] = ak[1] = ak[2] = 'disk'
-
-ao = {}
-ao[0] = ao[1] = ao[2] = []
+        if (format == 'csv'):
+            for y in data:
+                for item in y:
+                    if item != 'type':
+                       sys.stdout.write('"' + str(y[item]) + '";')
+                print '"' + y['type'] + '"'
 
 
-#
-# strace -> csv
-#
+def strace2info():
+        global format
 
-for line in sys.stdin:
+	# 
+	# Names (an), Types (ak), and Operations (ao) 
+	# 
 
-    x = re.split(r'\s*[<>(),=\n]\s*', line)
-    l_date = x[0].split()[0]
-    l_op   = x[0].split()[1]
+	an = {} 
+	an[0] = '"stdin"'
+	an[1] = '"stdout"'
+	an[2] = '"stderr"'
+
+	ak = {} 
+	ak[0] = ak[1] = ak[2] = 'disk'
+
+	ao = {}
+	ao[0] = ao[1] = ao[2] = []
 
 
-    #
-    # Start: I/O
-    #
+	#
+	# strace -> csv
+	#
 
-    if l_op == 'open':
+	for line in sys.stdin:
+
+	    x = re.split(r'\s*[<>(),=\n]\s*', line)
+	    l_date = x[0].split()[0]
+	    l_op   = x[0].split()[1]
+
+
+	    #
+	    # Start: I/O
+	    #
+
+	    if l_op == 'open':
+		try:
+		   key = int(x[4])
+		except ValueError:
+		   try:
+		      key = int(x[5])
+		   except ValueError:
+		      continue
+		except KeyError:
+		   continue
+
+                data = { "type":      "disk", 
+                         "date":      l_date,
+                         "x_2":       x[len(x)-2],
+                         "op":        l_op,
+                         "x1":        x[1] } ; 
+
+		ak[key] = 'disk'
+		an[key] = x[1] 
+		ao[key] = [ data ]
+
+
+	    if l_op == 'openat':
+		try:
+		   key = int(x[5])
+		except ValueError:
+		   continue
+
+                data = { "type":      "disk", 
+                         "date":      l_date,
+                         "x_2":       x[len(x)-2],
+                         "op":        l_op,
+                         "x2":        x[2] } ; 
+
+		ak[key] = 'disk'
+		an[key] = x[2] 
+		ao[key] = [ data ]
+
+
+	    #
+	    # Start: network
+	    #
+
+	    if l_op == 'socket':
+		try:
+		   key  = int(x[5])
+                   data = { "type":      "network", 
+                            "date":      l_date,
+                            "duration":  x[-2],
+                            "op":        l_op,
+                            "key":       str(key) } ; 
+		except ValueError:
+		   continue
+
+		ak[key] = 'network'
+		an[key] = key 
+		ao[key] = [ data ]
+
+
+	    if l_op == 'connect':
+		try:
+		   key = int(x[1])
+		   l_dest = ''
+		   l_notation = x[4]
+
+		   if l_notation == 'sun_path':
+		      l_dest = x[5]
+   		   if l_notation == 'sin_port':
+		      l_dest = x[10] + ':' + x[6]
+		   l_dest = l_dest.replace("\"","")
+		   l_dest = l_dest.replace("}","")
+
+                   data = { "type":      "network", 
+                            "date":      l_date,
+                            "duration":  x[-2],
+                            "op":        l_op,
+                            "key":       str(key),
+                            "dest":      l_dest } ; 
+		except ValueError:
+		   continue
+
+		ao[key] += [ data ]
+
+
+	    #
+	    # Send/Receive data
+	    #
+
+	    if l_op == 'recvfrom':
+		try:
+		   key = int(x[1])
+		   if key not in ao:
+		      continue
+
+		   l_dest = 'NULL'
+		   if x[5] != 'NULL':
+		      l_dest = x[13] + ':' + x[9]
+		      l_dest = l_dest.replace("\"","")
+
+                   data = { "type":      "network", 
+                            "date":      l_date,
+                            "duration":  x[-2],
+                            "op":        l_op,
+                            "key":       str(an[key]),
+                            "dest":      l_dest } ; 
+		except ValueError:
+		   continue
+
+		ao[key] += [ data ]
+
+
+	    if l_op == 'sendmmsg':
+		try:
+		   key = int(x[1])
+		   if key not in ao:
+		      continue
+
+                   data = { "type":      "network", 
+                            "date":      l_date,
+                            "duration":  x[-2],
+                            "op":        l_op,
+                            "an_key":    str(an[key]) } ; 
+		except ValueError:
+		   continue
+
+		ao[key] += [ data ]
+
+
+	    if l_op == 'read':
+		try:
+		   key = int(x[1])
+		   if key not in ao:
+		      continue
+		except ValueError:
+		   continue
+
+		if x[len(x)-5] == '-1 EISDIR':
+		   continue
+
+                data = { "type":      ak[key], 
+                         "date":      l_date,
+                         "duration":  x[-2],
+                         "op":        l_op,
+                         "an_key":    str(an[key]),
+                         "amount":    x[len(x)-3] } ; 
+		ao[key] += [ data ]
+
+
+	    if l_op == 'write':
+		try:
+		   key = int(x[1])
+		   if key not in ao:
+		      continue
+		except ValueError:
+		   continue
+
+		if x[len(x)-5] == '-1 EISDIR':
+		   continue
+
+                data = { "type":      ak[key], 
+                         "date":      l_date,
+                         "duration":  x[-2],
+                         "op":        l_op,
+                         "an_key":    str(an[key]),
+                         "amount":    x[len(x)-3] } ; 
+		ao[key] += [ data ]
+
+
+	    #
+	    # Stop
+	    #
+
+	    if l_op == 'close':
+		try:
+		   key = int(x[1])
+		   if key not in ao:
+		      continue
+
+		   l_name = str(an[key])
+		   l_name = l_name.replace("\"","")
+
+                   data = { "type":      ak[key], 
+                            "date":      l_date,
+                            "duration":  x[-2],
+                            "op":        l_op,
+                            "name":      l_name } ; 
+		except ValueError:
+		   continue
+		except KeyError:
+		   continue
+
+		ao[key] += [ data ]
+
+                print_records(format, ao[key])
+
+		ak[key] = ''
+		an[key] = ''
+		ao[key] = [] 
+
+	# print "\n"
+
+	for key in ao:
+	    if an[key] != '':
+               print_records(format, ao[key])
+
+
+def main(argv):
+        global format
+
+        # get parameters
         try:
-           key = int(x[4])
-        except ValueError:
-           try:
-              key = int(x[5])
-           except ValueError:
-              continue
-        except KeyError:
-           continue
+           opts, args = getopt.getopt(argv,"hf",["format="])
+        except getopt.GetoptError:
+           print 'strace2csv.sh -f <format>'
+           sys.exit(2)
 
-        ak[key] = 'disk'
-        an[key] = x[1] 
-        ao[key] = ['"disk";"' + l_date + '";"' + x[len(x)-2] + '";"' + l_op + '";' + x[1]] 
+        for opt, arg in opts:
+            if opt == '-h':
+               print 'app2csv.sh -f <format>'
+               sys.exit()
+            elif opt in ("-f", "--format"):
+               format  = str(arg)
 
-
-    if l_op == 'openat':
-        if x[5] == '-1 EACCES' or x[5] == '-1 ENOENT':
-           continue
-
-        try:
-           key = int(x[5])
-        except ValueError:
-           continue
-
-        ak[key] = 'disk'
-        an[key] = x[2] 
-        ao[key] = ['"disk";"' + l_date + '";"' + x[len(x)-2] + '";"' + l_op + '";' + x[2]] 
+	# start transformation
+	strace2info()
 
 
-    #
-    # Start: network
-    #
+# initial values
+format = 'csv'
 
-    if l_op == 'socket':
-        try:
-           key = int(x[5])
-           l_duration = x[-2]
-        except ValueError:
-           continue
-
-        ak[key]  = 'network'
-        an[key]  = key 
-        ao[key]  = ['"network";"' + l_date + '";"' + l_duration + '";"' + l_op + '";"' + str(key) + '"'] 
-
-
-    if l_op == 'connect':
-        try:
-           key = int(x[1])
-           l_duration = x[-2]
-           l_notation = x[4]
-        except ValueError:
-           continue
-
-        l_dest = ''
-        if l_notation == 'sun_path':
-           l_dest = x[5]
-        if l_notation == 'sin_port':
-           l_dest = x[10] + ':' + x[6]
-        l_dest = l_dest.replace("\"","")
-        l_dest = l_dest.replace("}","")
-
-        ao[key] += ['"network";"' + l_date + '";"' + l_duration + '";"' + l_op + '";"' + str(key) + '";"' + l_dest + '"'] 
-
-
-    #
-    # Send/Receive data
-    #
-
-    if l_op == 'recvfrom':
-        try:
-           key = int(x[1])
-           l_duration = x[-2]
-           l_dest = 'NULL'
-           if x[5] != 'NULL':
-              l_dest = x[13] + ':' + x[9]
-              l_dest = l_dest.replace("\"","")
-        except ValueError:
-           continue
-
-        if key not in ao:
-           continue
-
-        ao[key] += ['"network";"' + l_date + '";"' + l_duration + '";"' + l_op + '";"' + str(an[key]) + '";"' + l_dest + '"']
-
-
-    if l_op == 'sendmmsg':
-        try:
-           key = int(x[1])
-           l_duration = x[-2]
-        except ValueError:
-           continue
-
-        if key not in ao:
-           continue
-
-        ao[key] += ['"network";"' + l_date + '";"' + l_duration + '";"' + l_op + '";"' + str(an[key]) + '";"' + '"'] 
-
-
-    if l_op == 'read':
-        try:
-           key = int(x[1])
-           l_duration = x[-2]
-        except ValueError:
-           continue
-
-        if x[len(x)-5] == '-1 EISDIR':
-           continue
-        if key not in ao:
-           continue
-
-        ao[key] += [ '"' + ak[key] + '";"' + l_date + '";"' + x[len(x)-2] + '";"' + l_op + '";' + str(an[key]) + ';"' + x[len(x)-3] + '"'] 
-
-
-    if l_op == 'write':
-        try:
-           key = int(x[1])
-           l_duration = x[-2]
-        except ValueError:
-           continue
-
-        if x[len(x)-5] == '-1 EISDIR':
-           continue
-        if key not in ao:
-           continue
-
-        ao[key] += [ '"' + ak[key] + '";"' + l_date + '";"' + l_duration + '";"' + l_op + '";' + str(an[key]) + ';"' + x[len(x)-3] + '"'] 
-
-
-    #
-    # Stop
-    #
-
-    if l_op == 'close':
-        try:
-           key = int(x[1])
-           l_duration = x[-2]
-           l_name = str(an[key])
-           l_name = l_name.replace("\"","")
-        except ValueError:
-           continue
-        except KeyError:
-           continue
-
-        if key not in ao:
-           continue
-
-        ao[key] += [ '"' + ak[key] + '";"' + l_date + '";"' + l_duration + '";"' + l_op + '";"' + l_name + '"' ]
-
-        for y in ao[key]:
-            print "{0}".format(y)
-
-        ak[key] = ''
-        an[key] = ''
-        ao[key] = [] 
-
-# print "\n"
-
-for key in ao:
-    if an[key] != '':
-       for y in ao[key]:
-           print "{0}".format(y)
-
+if __name__ == "__main__":
+   main(sys.argv[1:])
 
