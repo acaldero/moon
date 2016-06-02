93a56bcc521e9d6324e44b4ba81f133ad8c49181
Alejandro <acaldero.uc3m@gmail.com>
2016-06-02 18:06:27 +0200

    viewer-googlechart-timeline updated

--- node-mon-nc.sh
+++ node-mon-nc.sh
@@ -1,23 +1,31 @@
 #!/bin/sh
 
-if [ $# -ne 1 ]; then
+if [ $# -gt 1 ]; then
    echo "Usage: $0 <port>"
    exit
 fi
 
+if [ $# -eq 1 ]; then
+   PORT=$1
+else
+   PORT=9999
+fi
+
+echo "$0 running on port $PORT..."
+
 while [ 1 ]; do
 
-	./node-mon.py -f json -d 0.3 | awk 'BEGIN { 
-						    print "HTTP/1.1 200 OK"; 
-						    print "Access-Control-Allow-Origin: *"; 
-						    print "Content-Type: text/event-stream"; 
-						    print "Cache-Control: no-cache"; 
-						    print ""; 
-						  } 
-						  { 
+	./node-mon.py -f json -d 0.5 | awk 'BEGIN {
+						    print "HTTP/1.1 200 OK";
+						    print "Access-Control-Allow-Origin: *";
+						    print "Content-Type: text/event-stream";
+						    print "Cache-Control: no-cache";
+						    print "";
+						  }
+						  {
 						    print "data: " $0 "\n";
-						    fflush(); 
-						  }' | ( nc -l -p $1 ; killall awk )
+						    fflush();
+						  }' | ( nc -l -p $PORT ; killall awk )
 
 done
 
