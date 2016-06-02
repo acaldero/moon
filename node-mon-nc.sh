a28f5066279216688a9a496354eb55903d307b76
Alejandro <acaldero.uc3m@gmail.com>
2016-06-02 08:09:24 +0200

    End upon close connection

--- node-mon-nc.sh
+++ node-mon-nc.sh
@@ -5,14 +5,19 @@ if [ $# -ne 1 ]; then
    exit
 fi
 
-./node-mon.py -f json -d 0.3 | awk 'BEGIN { 
-                                            print "HTTP/1.1 200 OK"; 
-                                            print "Access-Control-Allow-Origin: *"; 
-                                            print "Content-Type: text/event-stream"; 
-                                            print "Cache-Control: no-cache"; 
-                                            print ""; 
-                                          } 
-                                          { 
-                                            print "data: " $0 "\n";
-                                            fflush(); 
-                                          }' | ( nc -l -p $1 ; killall awk )
+while [ 1 ]; do
+
+	./node-mon.py -f json -d 0.3 | awk 'BEGIN { 
+						    print "HTTP/1.1 200 OK"; 
+						    print "Access-Control-Allow-Origin: *"; 
+						    print "Content-Type: text/event-stream"; 
+						    print "Cache-Control: no-cache"; 
+						    print ""; 
+						  } 
+						  { 
+						    print "data: " $0 "\n";
+						    fflush(); 
+						  }' | ( nc -l -p $1 ; killall awk )
+
+done
+
